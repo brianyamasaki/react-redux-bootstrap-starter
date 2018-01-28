fd88e40aaaa77a2eccbb5865328ff4b9ff82ae28
Brian Yamasaki <brian.yamasaki@gmail.com>
2018-01-27 21:55:21 -0800

    cleaned up react-bootstrap and added fetch

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Grid, Row } from 'react-bootstrap';
 
 export default () => (
-  <div>
-    <h1>About Us</h1>
+  <Grid>
+    <Row>
+    <h1 className="text-center">About Us</h1>
     <p>Hello Medium!</p>
-  </div>
+    </Row>
+  </Grid>
 )
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import { Grid, Row, Button } from 'react-bootstrap';
 
 const Home = props => (
-  <div>
-    <h1>Home</h1>
+  <Grid>
+    <Row>
+    <h1 className="text-center">Home</h1>
     <p>Welcome home!</p>
-    <button onClick={() => props.changePage()}>Go to about page via redux</button>
-  </div>
+    <div className="text-center">
+      <Button onClick={() => props.changePage()}>Go to about page via redux</Button>
+    </div>
+    </Row>
+  </Grid>
 )
 
 const mapDispatchToProps = dispatch => bindActionCreators({
